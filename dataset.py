870277246f4ba1f081755acbd276349504467ff6
DONG Xiangjing <dxj98@foxmail.com>
2022-03-13 00:00:46 +0100

    finish __getitem__ func in LinemodDataset

--- dataset.py
+++ dataset.py
@@ -1,4 +1,7 @@
+# The code is adapted from https://github.com/j96w/DenseFusion/blob/master/datasets/linemod/dataset.py
+
 import os
+import torch
 import numpy as np
 from PIL import Image
 import torchvision.transforms as transforms
@@ -7,9 +10,10 @@ import yaml
 
 
 class LinemodDataset(data.Dataset):
-    def __init__(self, mode, dataset_path, obj_list=[1, 2, 4, 5, 6, 8, 9, 10, 11, 12, 13, 14, 15]):
+    def __init__(self, mode, dataset_path, cloud_pt_num, obj_list=[1, 2, 4, 5, 6, 8, 9, 10, 11, 12, 13, 14, 15]):
         self.mode = mode
         self.objects = obj_list
+        self.cloud_pt_num = cloud_pt_num
         self.sys_obj = [7, 8]
 
         self.rgb_list = []        # path to rgb img
@@ -73,9 +77,9 @@ class LinemodDataset(data.Dataset):
 
     def __getitem__(self, item):
         # read rgb img, depth img and mask
-        rgb_img = Image.open(self.rgb_list[item])
-        depth_img = Image.open(self.depth_list[item])
-        label_img = Image.open(self.label_list[item])
+        rgb_img = np.array(Image.open(self.rgb_list[item]))
+        depth_img = np.array(Image.open(self.depth_list[item]))
+        label_img = np.array(Image.open(self.label_list[item]))
 
         obj_id = self.obj_list[item]
         img_id = self.index_list[item]
@@ -89,16 +93,73 @@ class LinemodDataset(data.Dataset):
         else:
             gt = self.gt_dict[obj_id][img_id][0]
 
+        # output a point cloud
+        depth_mask = np.ma.getmaskarray(np.ma.masked_not_equal(depth_img, 0))    # mask non-zero as True
+        if self.mode == 'eval':
+            label_mask = np.ma.getmaskarray(np.ma.masked_equal(label_img, np.array(255)))   # mask 255 as True
+        else:
+            # mask 255，255，255 as True, since the img has 3 channel
+            label_mask = np.ma.getmaskarray(np.ma.masked_equal(label_img, np.array([255, 255, 255])))[:, :, 0]
+        mask = label_mask * depth_mask
 
+        # get the bounding box and crop img
+        rmin, rmax, cmin, cmax = get_bb(gt['obj_bb'])
+        img_crop = rgb_img[rmin:rmax, cmin:cmax, :]
 
-        pass
+        # select cloud_pt_num points to make a point cloud
+        choice = mask[rmin:rmax, cmin:cmax].flatten().nonzero()[0]  # nonzero returns a tuple
+        if len(choice) == 0:
+            return None, None, None, None, None, None
+        if len(choice) > self.cloud_pt_num:
+            # randomly select cloud_pt_num points
+            choice = np.random.choice(choice, self.cloud_pt_num, replace=False)
+        # repeat points if not enough
+        else:
+            choice = np.pad(choice, (0, self.cloud_pt_num - len(choice)), 'wrap')
 
+        depth_points = depth_img[rmin:rmax, cmin:cmax].flatten()[choice].reshape(-1, 1)
+        x_points = self.xmap[rmin:rmax, cmin:cmax].flatten()[choice].reshape(-1, 1)
+        y_points = self.ymap[rmin:rmax, cmin:cmax].flatten()[choice].reshape(-1, 1)
+        choice = np.array([choice])
+
+        pt2 = depth_points / 1000
+        pt0 = (x_points - self.cam_cx) * pt2 / self.cam_fx
+        pt1 = (y_points - self.cam_cy) * pt2 / self.cam_fy
+        cloud = np.hstack((pt0, pt1, pt2))
+
+        # get ground truth rotation and translation
+        target_rotation = np.resize(np.array(gt['cam_R_m2c']), (3, 3))
+        target_translation = np.array(gt['cam_t_m2c']) / 1000
+
+        gt_translation = target_translation
+        target_translation -= cloud
+        target_translation /= np.linalg.norm(target_translation, axis=1)
+        target_translation = np.array([target_translation])
+
+        model_vtx = self.vtx_dict[obj_id] / 1000.0
+        vtx_choice = np.random.choice(len(model_vtx), self.num_pt_mesh, replace=False)
+        model_vtx = model_vtx[vtx_choice, :]
+        target_rotation = np.dot(model_vtx, target_rotation.T)
+
+        return (torch.from_numpy(cloud.astype(np.float32)),
+                torch.LongTensor(choice.astype(np.int32)),
+                self.transform(img_crop),
+                torch.from_numpy(target_translation.astype(np.float32)),
+                torch.from_numpy(target_rotation.astype(np.float32)),
+                torch.from_numpy(model_vtx.astype(np.float32)),
+                torch.LongTensor([self.objects.index(obj_id)]),
+                torch.from_numpy(gt_translation.astype(np.float32)))
 
     def __len__(self):
        return len(self.rgb_list)
 
 
 def read_ply_vtx(filepath):
+    """
+    get the vertexes of a model
+    :param filepath: the path of ply file
+    :return: an array of size (num_vtx, 3)
+    """
     f = open(filepath)
     assert f.readline().strip() == "ply"
     f.readline()
@@ -110,3 +171,7 @@ def read_ply_vtx(filepath):
     for _ in range(N):
         pts.append(np.float32(f.readline().split()[:3]))
     return np.array(pts)
+
+
+def get_bb(bb):
+   pass
